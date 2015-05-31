135406c34e2f6b3a12dc4332b4f1a0a5d26990b2
Parsa Alipour <parsaalipour@hotmail.com>
2015-05-30 22:45:32 -0400

    initial commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Parsa Alipour on 2015-05-30.
+//  Copyright (c) 2015 Parsa Alipour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ToDoList
+//
+//  Created by Parsa Alipour on 2015-05-30.
+//  Copyright (c) 2015 Parsa Alipour. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
